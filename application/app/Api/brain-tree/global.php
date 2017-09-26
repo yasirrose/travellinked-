46675f43e58a643b5c8a97373314b563130e8f7f
Yasir <yasirrose@hotmail.com>
2017-09-26 18:17:35 +0500

    config

--- application/app/Api/brain-tree/global.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	include 'lib/Braintree.php';
-	Braintree_Configuration::environment('sandbox');
-	Braintree_Configuration::merchantId('s3jyd57pjg7fpdqh');
-	Braintree_Configuration::publicKey('7hxf3bnd6dwdbn4w');
-	Braintree_Configuration::privateKey('32403f0f352aadae4b735949a479c21e');
-?>
